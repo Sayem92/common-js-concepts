5e270e41b535ee7de0e360ed82fcd7ec5eb910f7
Sayem92 <sayemmiha123@gmail.com>
2022-09-08 12:29:09 +0600

    add closure function into function

---
+++ closure.js
@@ -0,0 +1,17 @@
+function kitchen(){
+    let roast = 0
+    return function (){
+        roast++
+        return roast
+    }
+}
+const firstServer = kitchen()
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+console.log(firstServer())
+
+
+
+// note the closure most important
